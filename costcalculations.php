f5de9285e8dfab5535560d87030f5a81dd69b806
Amit Sharma <lettertoamit@users.noreply.github.com>
2019-09-17 14:51:56 +0530

    Update and rename index.php to costcalculations.php

--- costcalculations.php
+++ costcalculations.php
@@ -2,11 +2,13 @@
 
 /*
 authour : amit sharma 
-Description : sample code for lulu.com Api  sanbox mode 
+Description : sample code for lulu.com Api  sanbox mode for caost calculations 
 */
 use GuzzleHttp\Client;
 
 require 'vendor/autoload.php';
+
+// authentication for O auth 2 
 $client = new Client(['base_uri'=>"https://api.sandbox.lulu.com/"]);
 $response = $client->request('POST', 
 	'auth/realms/glasstree/protocol/openid-connect/token',
@@ -41,28 +43,6 @@ $array = array("line_items"=>array( array(
 "shipping_level" => "EXPRESS"
 );
 $array = json_encode($array);
-// $v = '{
-//     "line_items": [
-//         {
-//             "page_count": 32,
-//             "pod_package_id": "0600X0900BWSTDPB060UW444MXX",
-//             "quantity": 20
-//         },
-//         {
-//             "page_count": 324,
-//             "pod_package_id": "0425X0687BWSTDLW060UC444GRB",
-//             "quantity": 200
-//         }
-//     ],
-//     "shipping_address": {
-//         "city": "Washington",
-//         "country_code": "US",
-//         "postcode": "20540",
-//         "state_code": "DC",
-//         "street1": "101 Independence Ave SE"
-//     },
-//     "shipping_level": "EXPRESS"
-// }'; 
 $response = $client->request('POST', 
 	'print-job-cost-calculations/',
 	 ['headers' =>[ 
